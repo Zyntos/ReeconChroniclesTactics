f33d7f329e6a69340d0bce6910dfb2fb75b5052b
Zyntos <zyrielsleepwalker@gmail.com>
2015-10-29 16:31:46 +0100

    functions und main PHP Teil-1.0

---
+++ Code/main.php
@@ -0,0 +1,24 @@
+<?php
+####################
+# Lifepoints
+$lp1=20;
+$lp2=20;
+####################
+# Decklists
+$deck1= array("");
+$deck2= array("");
+#Hier werden die Kartennummern in die Arrays eingetragen und damit Decks zugeordnet,
+#sobald die SQL Datenbank steht.
+####################
+
+# Aktiver Spieler
+$player1=true;
+
+
+
+
+
+
+
+
+?>
