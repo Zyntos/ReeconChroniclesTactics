4a36bd4bae977fce2faa937fd2b45ce38f910263
Zyntos <zyrielsleepwalker@gmail.com>
2015-11-05 21:23:29 +0100

    highlighted klickbare Felder auf Knopfdruck

--- Code/jsfunctions.js
+++ Code/jsfunctions.js
@@ -1,70 +1,74 @@
 var player = 1;
 var player1 = 0;
+var player1loc = 0;
 var player2 = 0;
 var movePhase1 = 0;
+var field = document.getElementsByClassName("Feld");
+var playersFound = 0;
 
+var i=0;
+var f=0;
 
 
 
 
 function movePhase() {
 	movePhase1 = 1;
-	´
-	
+  
 }
 
-function findPlayer(p) {
-	window.alert(p);
-	for (var i=0; i<document.getElementsByClassName("Feld").length; i++) {
-		if (p==1) {
-			
-			if (document.getElementsByClassName("Feld")[i].src=="red.jpg") {
-				player1 = document.getElementsByClassName("Feld")[i].Id;
-				window.alert(player1);
-		    }
-		}	
-		else {
-			if (document.getElementsByClassName("Feld")[i].src=="green.jpg") {
-				player2 = document.getElementsByClassName("Feld")[i].Id;
-				window.alert(player2);
-			}
-		}	
-			
-		}
-		
-}
+
 	
 
 
 function highlight() {
 	if (movePhase1==1) {
-		window.alert(movePhase1); 
-		findPlayer(player)
-        for (var i=0;i<length; i++) {
-			if(document.getElementsByClassName("Feld")[i].Id==player1+1+"") {
-				document.getElementsByClassName("Feld")[i].src=highlighted.jpg;
+		
+		findPlayer();
+		playersFound=1;
+		for (f=0;f<field.length; f++) {
+			if(field[f].id==player1-(-1)+"") {
+				
+				field[f].src="highlighted.jpg";
+				
 			}
-			if(document.getElementsByClassName("Feld")[i].Id==player1+2+"") {
-				document.getElementsByClassName("Feld")[i].src=highlighted.jpg;
+			if(field[f].id==player1-(-2)+"") {
+				
+				field[f].src="highlighted.jpg";
+				
 			}
-			if(document.getElementsByClassName("Feld")[i].Id==player1+10+"") {
-				document.getElementsByClassName("Feld")[i].src=highlighted.jpg;
+			if(field[f].id==player1-(-10)+"") {
+				
+				field[f].src="highlighted.jpg";
+				
 			}
-			if(document.getElementsByClassName("Feld")[i].Id==player1+20+"") {
-				document.getElementsByClassName("Feld")[i].src=highlighted.jpg;
+			if(field[f].id==player1-(-20)+"") {
+				
+				field[f].src="highlighted.jpg";
+				
 			}
-			if(document.getElementsByClassName("Feld")[i].Id==player1-1+"") {
-				document.getElementsByClassName("Feld")[i].src=highlighted.jpg;
+			if(field[f].id==player1-1+"") {
+				
+				field[f].src="highlighted.jpg";
+				
 			}
-			if(document.getElementsByClassName("Feld")[i].Id==player1-2+"") {
-				document.getElementsByClassName("Feld")[i].src=highlighted.jpg;
+			if(field[f].id==player1-2+"") {
+				
+				field[f].src="highlighted.jpg";
+							
 			}
-			if(document.getElementsByClassName("Feld")[i].Id==player1-10+"") {
-				document.getElementsByClassName("Feld")[i].src=highlighted.jpg;
+			if(field[f].id==player1-10+"") {
+				
+				field[f].src="highlighted.jpg";
+				
 			}
-			if(document.getElementsByClassName("Feld")[i].Id==player1-20+"") {
-				document.getElementsByClassName("Feld")[i].src=highlighted.jpg;
+			if(field[f].id==player1-20+"") {
+				
+				field[f].src="highlighted.jpg";
+				
+				
 			}
+			
 			
 				
 			
@@ -76,3 +80,30 @@ function highlight() {
 	}
 	
 }
+
+function findPlayer() {
+	
+	for (i=0; i<field.length; i++) {
+		
+		if (player==1) {
+			if (playersFound ==0){
+			if (field[i].src=="file:///C:/Users/Darren/Desktop/STUDIUM%20PRG/ReeconChroniclesTactics/Code/red.jpg") {
+				player1 = field[i].id;
+				
+				
+				}
+			
+		else {
+			if (field[i].src=="file:///C:/Users/Darren/Desktop/STUDIUM%20PRG/ReeconChroniclesTactics/Code/green.jpg") {
+				player2 = field[i].id;
+				
+				
+			}
+		}	
+		
+		}
+	}	}	
+	}
+		
+		
+
