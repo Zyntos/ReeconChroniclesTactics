39782571e79ab24b77fae391428e045e6e06bb12
Zyntos <zyrielsleepwalker@gmail.com>
2015-11-15 14:51:44 +0100

    moveFunktion hinzugefügt

--- Code/jsfunctions.js
+++ Code/jsfunctions.js
@@ -5,6 +5,7 @@ var player2 = 0;
 var movePhase1 = 0;
 var field = document.getElementsByClassName("Feld");
 var playersFound = 0;
+var highlighted = false;
 
 var i=0;
 var f=0;
@@ -14,6 +15,8 @@ var f=0;
 
 function movePhase() {
 	movePhase1 = 1;
+	window.alert(movePhase1);
+	highlight();
   
 }
 
@@ -71,7 +74,7 @@ function highlight() {
 			
 			
 				
-			
+		highlighted = true;	
 		}		
 			
 		
@@ -84,17 +87,21 @@ function highlight() {
 function findPlayer() {
 	
 	for (i=0; i<field.length; i++) {
-		
-		if (player==1) {
-			if (playersFound ==0){
-			if (field[i].src=="file:///C:/Users/Darren/Desktop/STUDIUM%20PRG/ReeconChroniclesTactics/Code/red.jpg") {
+		if (playersFound ==0) {
+			
+			if (player==1){
+				
+				
+			if (field[i].src=="file:///C:/Users/Darren/Documents/reec/Code/red.jpg") {
+				window.alert(field[i].src);
 				player1 = field[i].id;
+				window.alert(player1);
 				
 				
-				}
+			}}
 			
 		else {
-			if (field[i].src=="file:///C:/Users/Darren/Desktop/STUDIUM%20PRG/ReeconChroniclesTactics/Code/green.jpg") {
+			if (field[i].src=="file:///C:/Users/Darren/Documents/reec/Code/green.jpg") {
 				player2 = field[i].id;
 				
 				
@@ -102,8 +109,23 @@ function findPlayer() {
 		}	
 		
 		}
-	}	}	
+	}	}
+
+function move(clicked_id) {
+	window.alert(clicked_id);
+	if (player == 1) {
+		if (highlighted==true) {
+			window.alert("clicked");
+			if (document.getElementbyId(clicked_id).src=="file:///C:/Users/Darren/Documents/reec/Code/highlighted.jpg") {
+				document.getElementbyId(clicked_id).src=red.jpg;
+			
+			}
+		
+		}
 	}
+	
+}	
+	
 		
 		
 
